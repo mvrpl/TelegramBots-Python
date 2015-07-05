772cd5d9afcde15756fd7acb76e99e08c7e4f6f5
Marcos Vinícius Rodrigues <mvro.dev@gmail.com>
2015-07-05 20:29:54 -0300

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,10 +1,17 @@
 __author__ = 'Marcos'
 
-# Telegram Token API
-telegram_bot = '*****************'
+# Telegram Python Bot Token API
+telegram_bot_python = '********:***********************'
+
+# Telegram SMS Bot Token API
+telegram_bot_sms = '********:***********************'
 
 # Twitter Tokens
-consumer_key = "*****************"
-consumer_secret = "*****************"
-access_key = "*****************-*****************"
-access_secret = "*****************"
+consumer_key = "********:***********************"
+consumer_secret = "********:***********************"
+access_key = "********-********:***********************"
+access_secret = "********************************"
+
+# Sinch SMS
+key_sinch = '*********-****-****-****-************'
+secret_sinch = '******************=='
