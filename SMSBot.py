281b577b0047e0cff99cc3397749d3f0c8d84d37
Marcos Vinícius Rodrigues <mvro.dev@gmail.com>
2015-07-05 19:18:57 -0300

    Bot for send SMS with http://sinch.com API

---
+++ SMSBot.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from sinchsms import SinchSMS
+import tokens
+import pickle
+import json
+import requests
+import os
+from time import sleep
+
+if os.path.isfile('last_update_sms.txt') == True:
+    file = open('last_update_sms.txt', 'r')
+    last_update = pickle.load(file)
+else:
+    last_update = 0
+
+url = 'https://api.telegram.org/bot%s/' % tokens.telegram_bot_sms
+
+while True:
+    get_updates = json.loads(requests.get(url + 'getUpdates').content)
+    for update in get_updates['result']:
+        if last_update < update['update_id']:
+            last_update = update['update_id']
+            file = open('last_update_sms.txt', 'w')
+            pickle.dump(last_update, file)
+            file.close()
+            print(update['message']['text'])
+            try:
+                if u'/enviaSMS' in update['message']['text']:
+                    keyword = update['message']['text'].split(' ', 2)
+                    number = '+55'+keyword[1]
+                    message = keyword[2]
+                    client = SinchSMS(tokens.key_sinch, tokens.secret_sinch)
+                    client.send_message(number, message)
+                    requests.get(url + 'sendMessage', params=dict(chat_id=update['message']['chat']['id'], text="Enviando '%s' para %s" % (message, number) ))
+            except KeyError as e:
+                print(e)
+                continue
+    sleep(3)
