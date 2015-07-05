8f89319288c92b9d8e21909aaa26c754ab0c1a15
Marcos Vinícius Rodrigues <mvro.dev@gmail.com>
2015-07-05 00:41:30 -0300

    Insert your tokens in this file

---
+++ tokens.py
@@ -0,0 +1,10 @@
+__author__ = 'Marcos'
+
+# Telegram Token API
+telegram_bot = '*****************'
+
+# Twitter Tokens
+consumer_key = "*****************"
+consumer_secret = "*****************"
+access_key = "*****************-*****************"
+access_secret = "*****************"
