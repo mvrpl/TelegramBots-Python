b4c2bba528b2c3dbf4fbe2ac1506028537f7c29e
Marcos Vinícius Rodrigues <mvro.dev@gmail.com>
2015-07-14 10:34:56 -0300

    Added account info details command by username.

--- InstaBot.py
+++ InstaBot.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import locale
 import os
 import pickle
 import random
@@ -9,6 +10,41 @@ import requests
 import tokens
 import json
 from time import sleep
+from mechanize import Browser
+from bs4 import BeautifulSoup
+
+locale.setlocale(locale.LC_ALL, 'pt_BR.utf-8')
+
+def find_between(s, first, last):
+    try:
+        start = s.index(first) + len(first)
+        end = s.index(last, start)
+        return s[start:end]
+    except ValueError:
+        return ""
+
+def AccountInfo(username):
+    br = Browser()
+    br.set_handle_robots(False)
+    br.open('https://instagram.com/'+username)
+    soup = BeautifulSoup(br.response().read())
+    body_tag = soup.body
+    data = body_tag.find("script", {"type": "text/javascript"}).string
+    json_output = find_between(data, "window._sharedData = ", ";")
+    user = json.loads(json_output)
+    follows = user['entry_data']['ProfilePage'][0]['user']['follows']['count']
+    followed_by = user['entry_data']['ProfilePage'][0]['user']['followed_by']['count']
+    media_count = user['entry_data']['ProfilePage'][0]['user']['media']['count']
+    bio = user['entry_data']['ProfilePage'][0]['user']['biography']
+    is_private = user['entry_data']['ProfilePage'][0]['user']['is_private']
+    private = 'Yes/Sim' if is_private == True else u'No/Não'
+    is_verified = user['entry_data']['ProfilePage'][0]['user']['is_verified']
+    verified = 'Yes/Sim' if is_verified == True else u'No/Não'
+    try:
+        user_id = user['entry_data']['ProfilePage'][0]['user']['id']
+    except:
+        user_id = 'None'
+    return u'Following/Seguindo: %s\nFollowers/Seguidores: %s\nMedia Count/Número de fotos/vídeos: %s\nBio: %s\nPrivate Profile/Perfil Privado: %s\nVerified Profile/Perfil Verificado: %s\nUser ID/ID do usuário: %s' % (locale.format('%d', int(follows), 1), locale.format('%d', int(followed_by), 1), media_count, bio, private, verified, user_id)
 
 if os.path.isfile('last_update_insta.txt') == True:
     file = open('last_update_insta.txt', 'r')
@@ -49,9 +85,15 @@ while True:
                 pass
             try:
                 if u'/start' in update['message']['text']:
-                    help_bot_en = u'Hi\nAvailable commands:\n/hashtag dogs to sending random photo with hashtag.\n\nLocation: Send your location to get a photo near you.\n\n'
-                    help_bot = u'Olá\nComandos disponíveis:\n/hashtag riodejaneiro para enviar uma foto aleatória com a hashtag.\n\nLocalização: Envie sua localização para receber uma foto perto de você.'
+                    help_bot_en = u'Hi\nAvailable commands:\n/hashtag dogs to sending random photo with hashtag.\n/info marcos_dev: Send account info.\nLocation: Send your location to get a photo near you.\n\n'
+                    help_bot = u'Olá\nComandos disponíveis:\n/hashtag riodejaneiro para enviar uma foto aleatória com a hashtag.\n/info marcos_dev: Envia infos do usuário.\nLocalização: Envie sua localização para receber uma foto perto de você.'
                     requests.get(url + 'sendMessage', params=dict(chat_id=update['message']['chat']['id'], text=help_bot_en+help_bot ))
+                if u'/info' in update['message']['text']:
+                    keyword = update['message']['text'].split(' ', 1)
+                    try:
+                        requests.get(url + 'sendMessage', params=dict(chat_id=update['message']['chat']['id'], text=AccountInfo(keyword[1]) ))
+                    except:
+                        requests.get(url + 'sendMessage', params=dict(chat_id=update['message']['chat']['id'], text=u'Erro! Tente novamente mais tarde.' ))
                 if u'/hashtag' in update['message']['text']:
                     try:
                         keyword = update['message']['text'].split(' ', 1)
